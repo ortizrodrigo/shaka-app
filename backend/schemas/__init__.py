430fd0eb73d43e99e80ef594fa34393c18eac6a3
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2026-03-29 16:54:06 -0500

    feat(api): add ChatMemberAddSchema for member invitation

--- backend/schemas/__init__.py
+++ backend/schemas/__init__.py
@@ -1,4 +1,4 @@
-from schemas.chat_member import ChatMemberSchema
+from schemas.chat_member import ChatMemberSchema, ChatMemberAddSchema
 from schemas.chat import ChatSchema, ChatCreateSchema
 from schemas.message import MessageSchema
 from schemas.user import UserSchema, UserPublicSchema
--- backend/schemas/chat_member.py
+++ backend/schemas/chat_member.py
@@ -4,4 +4,8 @@ class ChatMemberSchema(Schema):
     id = fields.Int(dump_only=True)
     user_id = fields.Int(required=True)
     chat_id = fields.Int(required=True)
+    role = fields.Str(dump_only=True)
     joined_at = fields.DateTime(dump_only=True)
+
+class ChatMemberAddSchema(Schema):
+    user_id = fields.Int(required=True)
