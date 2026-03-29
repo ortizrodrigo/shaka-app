b652cae90e92e6a57355ad8f81b990dc6c6c685e
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2026-03-29 00:41:41 -0500

    feat(api): add chat members list scoped to a specific chat; only members of that chat can list its members

--- backend/resources/chat_member.py
+++ backend/resources/chat_member.py
@@ -1,5 +1,6 @@
 from flask.views import MethodView
-from flask_smorest import Blueprint
+from flask_smorest import Blueprint, abort
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from extensions import db
 from models import ChatMemberModel
@@ -9,13 +10,34 @@ blp = Blueprint("chat_members", __name__, description="Operations on Chat Member
 
 @blp.route("/chat-member/<int:chat_member_id>")
 class ChatMember(MethodView):
+  @jwt_required()
   @blp.response(200, ChatMemberSchema)
   def get(self, chat_member_id):
+    current_user_id = int(get_jwt_identity())
     chat_member = ChatMemberModel.query.get_or_404(chat_member_id)
+
+    membership = ChatMemberModel.query.filter_by(
+      chat_id=chat_member.chat_id,
+      user_id=current_user_id
+    ).first()
+    if not membership:
+      abort(403, message="You are not a member of this chat.")
+
     return chat_member
 
-@blp.route("/chat-member")
+
+@blp.route("/chat/<int:chat_id>/members")
 class ChatMemberList(MethodView):
+  @jwt_required()
   @blp.response(200, ChatMemberSchema(many=True))
-  def get(self):
-    return ChatMemberModel.query.all()
+  def get(self, chat_id):
+    current_user_id = int(get_jwt_identity())
+
+    membership = ChatMemberModel.query.filter_by(
+      chat_id=chat_id,
+      user_id=current_user_id
+    ).first()
+    if not membership:
+      abort(403, message="You are not a member of this chat.")
+
+    return ChatMemberModel.query.filter_by(chat_id=chat_id).all()
